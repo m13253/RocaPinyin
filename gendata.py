485778005ca7806fea75e0441f4a8a5c13f59738
Star Brilliant <m13253@hotmail.com>
2014-12-13 18:04:07 +0800

    Update Makefile

--- gendata.py
+++ gendata.py
@@ -1,5 +1,21 @@
 #!/usr/bin/env python3
 
+# Copyright (C) 2014  StarBrilliant <m13253@hotmail.com>
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Lesser General Public
+# License as published by the Free Software Foundation; either
+# version 3.0 of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public
+# License along with this program.  If not,
+# see <http://www.gnu.org/licenses/>.
+
 import sys
 import re
 import unicodedata
@@ -39,6 +55,7 @@ def parse_variant(desc):
 
 def main(_, identifier, ucs_gte, ucs_lte, fn_readings, fn_variants):
     ucs_gte, ucs_lte = int(ucs_gte, 16), int(ucs_lte, 16)
+    sys.stdout.write('/*\n  Copyright (C) 2014  StarBrilliant <m13253@hotmail.com>\n\n  This program is free software; you can redistribute it and/or\n  modify it under the terms of the GNU Lesser General Public\n  License as published by the Free Software Foundation; either\n  version 3.0 of the License, or (at your option) any later version.\n\n  This program is distributed in the hope that it will be useful,\n  but WITHOUT ANY WARRANTY; without even the implied warranty of\n  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU\n  Lesser General Public License for more details.\n\n  You should have received a copy of the GNU Lesser General Public\n  License along with this program.  If not,\n  see <http://www.gnu.org/licenses/>.\n*/\n\n')
     sys.stdout.write('static const struct rocapinyin::char_data_t %s = {\n' % identifier)
 
     pinyin_table = {}
